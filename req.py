3d03616c086e487b83aa0cd002dff2dfb89fc7d8
AnkushSharma2698 <ankush1@ualberta.ca>
2021-01-11 08:39:26 -0700

    prints source code of script from github raw url

--- req.py
+++ req.py
@@ -4,6 +4,8 @@ def test_requests():
     print(requests.__version__)
     response = requests.get("http://google.com")
     print(response.status_code)
+    response = requests.get("https://raw.githubusercontent.com/AnkushSharma2698/CMPUT404Lab1/main/req.py")
+    print(response.text)
 
 if __name__ == "__main__":
     test_requests()
