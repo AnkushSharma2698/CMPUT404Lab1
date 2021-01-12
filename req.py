9282bb1e4a5fb0a12250c355ddba04d3a259b8d4
AnkushSharma2698 <ankush1@ualberta.ca>
2021-01-11 18:41:47 -0700

    fix: removed useless prints

--- req.py
+++ req.py
@@ -1,9 +1,6 @@
 import requests
 
 def test_requests():
-    print(requests.__version__)
-    response = requests.get("http://google.com")
-    print(response.status_code)
     response = requests.get("https://raw.githubusercontent.com/AnkushSharma2698/CMPUT404Lab1/main/req.py")
     print(response.text)
 
